93f61a9f74cb18537b17014743bd16abc9260119
李云聪 <294265421@qq.com>
2015-11-13 20:16:45 +0800

    加入了冒泡排序算法

---
+++ src/main/java/com/liyuncong/sort/sort_common/impl/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.liyuncong.sort.sort_common.impl;
+
+import com.liyuncong.sort.sort_common.Sort;
+
+public class BubbleSort implements Sort {
+
+	@Override
+	public <T extends Comparable<T>> void sort(T[] a) {
+		int len = a.length;
+		// 针对前i个元素
+		for(int i = len - 1; i >= 1; i--) {
+			// 把最大元素放到最后，也就是沉下去
+			for(int j = 0; j < i; j++) {
+				if (a[j].compareTo(a[j + 1]) > 0) {
+					T temp = a[j + 1];
+					a[j + 1] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Integer[] arr = new Integer[]{0, 3, 1, 5, 12, 7};
+		new BubbleSort().sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
